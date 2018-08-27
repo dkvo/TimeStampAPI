5a38751f918f49874acbb44ae9a2eaa7bdaf0bae
dkvo <dangkhoanvo@gmail.com>
2018-08-26 23:11:33 -0700

    change production port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const validate = require('validator');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = process.env.NODE_ENV || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
